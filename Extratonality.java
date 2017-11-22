87583f5f795caab7aeaab2cc86c8adf8ff3648ca
Savino <asavino@wustl.edu>
2017-11-22 14:33:00 -0600

    added different testing options

--- Extratonality.java
+++ Extratonality.java
@@ -1,5 +1,6 @@
 import javax.sound.midi.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /*
@@ -7,19 +8,69 @@ import java.io.IOException;
  * them by generating a note count for each file
  */
 public class Extratonality {
-    public static void main(String[] args) {
+
+    public static int[] keySigs =     {0,  0,  0,  0,
+                                 2,  2,  2,  2,
+                                 3,  3,  3,  3,
+                                 10, 3,  10, 10,
+                                 3,  3,  3,  0,
+                                 5, 10,  5,  5, 5,
+                                 9,  0,  5,  9,
+                                 5, 10,  5,  5,
+                                 5,  5,  10, 2, // last movement questionable
+
+    };
+
+    public static int[] genKeySigs = {0,0,0,0, // 1
+                                2,2,2,2,       // 2
+                                3,3,3,3,       // 3
+                                10,10,10,10,    //4
+                                3,3,3,3,        //5
+                                5,5,5,5,5,      //6
+                                9,9,9,9,        //7
+                                5,5,5,5,        //8
+                                5,5,5,5};       //9
+
+    public static void main(String[] args) throws IOException {
+        String[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
+                "A", "A#", "Error"};
+
+
         File dir = new File("./Midi_Files");
         File[] files = dir.listFiles((d, name) -> name.endsWith(".mid"));
+        Song[] songs = new Song[files.length];
 
-        for (File f : files) {
-            //TODO: parse each file and make them into songs
+        //process files into Song object
+        for (int i = 0; i<files.length; i++) {
+            songs[i] = processFile(files[i], i);
         }
+
+        //export data to CSV file
+        FileWriter writer = new FileWriter("Midi_results_generic_keys.csv");
+        writer.append("Title,C,C#,D,D#,E,F,F#,G,G#,A,A#,B,Key,Score\n");
+        for(Song s: songs){
+            writer.append(s.name);
+            for(int c: s.noteCounts){
+                writer.append(',');
+                writer.append(Integer.toString(c));
+            }
+            writer.append(',');
+            writer.append(Integer.toString(s.key));
+            writer.append(',');
+            writer.append(Float.toString(s.score));
+            writer.append('\n');
+        }
+        writer.flush(); writer.close();
+
+
     }
 
-    public Song processFile(File f) {
-        // TODO
-        // in here I'll more or less copy the ReadFile stuff and refactor it
-        // maybe make readFile return an array, and I'll use that to make the song?
+    /*
+     * input - midi file
+     * parses input file, and creates a Song object which contains note count
+     * data, and resulting score based on the note distributions
+     */
+    public static Song processFile(File f, int index) {
         int NOTE_ON = 0x90;
         int NOTE_OFF = 0x80;
         int trackNumber = 0;
@@ -39,25 +90,47 @@ public class Extratonality {
             trackNumber++;
             for (int i = 0; i < track.size(); i++) {
                 MidiEvent event = track.get(i);
-                //   System.out.print("@" + event.getTick() + " ");
                 MidiMessage message = event.getMessage();
                 if (message instanceof ShortMessage) {
                     ShortMessage sm = (ShortMessage) message;
-                    //    System.out.print("Channel: " + sm.getChannel() + " ");
                     if (sm.getCommand() == NOTE_ON) {
                         int key = sm.getData1();
-                        int octave = (key / 12) - 1;
                         int note = key % 12;
-                        //String noteName = NOTE_NAMES[note];
-                        int velocity = sm.getData2();
-                        //    System.out.println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
                         notecounts[note]++;
                         x++;
                     }
                 }
             }
         }
-        Song song = new Song(f.getName(), notecounts);
+        int maxAt = 0; int secondMaxAt = 0;
+        for (int i = 0; i < notecounts.length; i++) {
+            if(notecounts[i] > notecounts[maxAt]){
+                secondMaxAt = maxAt; maxAt = i;
+            }
+            else if(notecounts[i] > notecounts[secondMaxAt]){
+                secondMaxAt = i;
+            }
+        }
+
+        // this takes the assumption the tonic and dominant are the most common
+        // tones heard in each movement
+        int key;
+        switch((maxAt - secondMaxAt + 12) % 12){
+            case 7:
+                key = secondMaxAt;
+                break;
+            case 5:
+                key = maxAt;
+                break;
+            default:
+                System.out.println((maxAt - secondMaxAt + 12) % 12);
+                System.out.println(f.getName());
+                key = 12;
+                break;
+        }
+
+
+        Song song = new Song(f.getName(), notecounts, genKeySigs[index]);
         return song;
     }
 }
--- Song.java
+++ Song.java
@@ -1,27 +1,37 @@
 public class Song {
-    public int[] notecounts;
-    public int score;
+    public int[] noteCounts;
+    public float score;
     public String name;
-
+    public int key;
+    public boolean isMajor;
 
     /*
      * Object that contains the name of the piece, array of note counts for the piece,
      * and any scores calculated for the Extratonality of the piece
      */
     public Song(){
-        notecounts = new int[12]; //by default, filled with zeros
+        noteCounts = new int[12]; //by default, filled with zeros
         score = 0;
         name = "";
     }
-    public Song(String name, int[] notecounts){
+    protected Song(String name, int[] noteCounts, int key){
         this.name = name;
-        this.notecounts = notecounts;
+        this.noteCounts = noteCounts;
+        this.key = key;
         score = getScore();
 
     }
 
-    private int getScore(){
-        //TODO
-        return 0;
+    private float getScore(){
+        if(key == 12) return 0;
+        int keyNotes = noteCounts[(0 + key) % 12] + noteCounts[(2 + key) % 12] +
+                noteCounts[(4 + key) % 12] + noteCounts[(5 + key) % 12] +
+                noteCounts[(7 + key) % 12] + noteCounts[(9 + key) % 12] +
+                noteCounts[(11 + key) % 12];
+        int nonKeyNotes = noteCounts[(1 + key) % 12] + noteCounts[(3 + key) % 12] +
+                noteCounts[(6 + key) % 12] + noteCounts[(8 + key) % 12] +
+                noteCounts[(10 + key) % 12];
+        return ((float) nonKeyNotes) / keyNotes;
+
     }
 }
